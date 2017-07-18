d7ec62dc90a4924b2a0c0192317605d2b1b757eb
dwgjjdxq <dwgjjdxq@gmail.com>
2017-07-18 10:09:00 +0800

    add test

--- myString/main.cpp
+++ myString/main.cpp
@@ -2,7 +2,7 @@
 
 int myStrlen(const char * str);
 char * myStrcpy(char * dest, const char * src);
-char * myStrcmp(const char * s1, const char * s2);
+int myStrcmp(const char * s1, const char * s2);
 char * myStrcat(char * dest, const char * src);
 
 int myStrlen(const char * str)
@@ -13,8 +13,34 @@ int myStrlen(const char * str)
 	return ret;
 }
 
+char * myStrcpy(char * dest, const char * src)
+{
+	char * ret = dest;
+	while (*dest++ = *src++);
+	return ret;
+}
 
+int myStrcmp(const char * s1, const char * s2)
+{
+	for (; s1 && s2 && (*s1 == *s2); ++s1, ++s2);
+	return (s1 == s2) ? 0 : (s1 > s2 ? 1 : -1);
+}
+
+char * myStrcat(char * dest, const char * src)
+{
+	char * ret = dest;
+	while (*dest)
+		++dest;
+	while (*dest++ = *src++);
+	return ret;
+}
 
 int main() {
+
+	// myStrlen
+	char * buf_1 = "china";
+	int len = myStrlen(buf_1);
+	printf("myStrlen(buf_1) = %d\n", len);
+
 	return 0;
 }
