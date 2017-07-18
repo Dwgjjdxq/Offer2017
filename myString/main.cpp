7beebf43a78025609261655c1c2ab24a5875b68b
dwgjjdxq <dwgjjdxq@gmail.com>
2017-07-18 10:26:21 +0800

    add all test

--- myString/main.cpp
+++ myString/main.cpp
@@ -23,7 +23,7 @@ char * myStrcpy(char * dest, const char * src)
 int myStrcmp(const char * s1, const char * s2)
 {
 	for (; s1 && s2 && (*s1 == *s2); ++s1, ++s2);
-	return (s1 == s2) ? 0 : (s1 > s2 ? 1 : -1);
+	return (*s1 == *s2) ? 0 : (*s1 > *s2 ? 1 : -1);
 }
 
 char * myStrcat(char * dest, const char * src)
@@ -41,6 +41,24 @@ int main() {
 	char * buf_1 = "china";
 	int len = myStrlen(buf_1);
 	printf("myStrlen(buf_1) = %d\n", len);
+
+	// myStrcpy
+	char buf_2[1024];
+	char buf_3[1024] = "china is great";
+	myStrcpy(buf_2, buf_3);
+	printf("buf_2 = %s\n", buf_2);
+
+	// myStrcmp
+	char buf_4[1024] = "abc";
+	char buf_5[1024] = "dbcef";
+	int ret_cmp = myStrcmp(buf_4, buf_5);
+	printf("myStrcmp = %d\n", ret_cmp);
+
+	// myStrcat
+	char buf_6[1024] = "china";
+	char buf_7[1024] = " is great";
+	char * ret_cat = myStrcat(buf_6, buf_7);
+	printf("ret_cat = %s\n", ret_cat);
 
 	return 0;
 }
