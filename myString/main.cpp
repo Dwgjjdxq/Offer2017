380886d5c5773fd7bd0c3f9cb690d9f9dba05c49
dwgjjdxq <dwgjjdxq@gmail.com>
2017-07-18 10:02:29 +0800

    add myStrlen

--- myString/main.cpp
+++ myString/main.cpp
@@ -5,6 +5,16 @@ char * myStrcpy(char * dest, const char * src);
 char * myStrcmp(const char * s1, const char * s2);
 char * myStrcat(char * dest, const char * src);
 
+int myStrlen(const char * str)
+{
+	int ret = 0;
+	while (*str++)
+		++ret;
+	return ret;
+}
+
+
+
 int main() {
 	return 0;
 }
