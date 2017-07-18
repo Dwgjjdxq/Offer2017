f8301fdf13bdc68bb7c0ee92c25bd08d53aaea14
dwgjjdxq <dwgjjdxq@gmail.com>
2017-07-18 09:57:15 +0800

    自实现字符串常用操作

---
+++ myString/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int myStrlen(const char * str);
+char * myStrcpy(char * dest, const char * src);
+char * myStrcmp(const char * s1, const char * s2);
+char * myStrcat(char * dest, const char * src);
+
+int main() {
+	return 0;
+}
