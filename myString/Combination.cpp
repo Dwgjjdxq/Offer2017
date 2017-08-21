c453c45b5b30aaec288aa1206ec71150fcf4a6fa
dwgjjdxq <dwgjjdxq@gmail.com>
2017-08-21 15:03:37 +0800

    add some notes

--- myString/Combination.cpp
+++ myString/Combination.cpp
@@ -62,8 +62,8 @@ void Combination2(char *str) {
 	int length = strlen(str);
 	int n = 1 << length;	// 2^n
 	for (int bitmap = 1; bitmap < n; ++bitmap) {	// 从1开始，共2^n-1种情况, 
-		for (int offset = 0; offset < length; ++offset) {	// 对1取偏移量offset时，默认从0开始,按位判断
-			if (bitmap & (1 << offset))	//bitmap=011,则j同过for循环依次取0和1，011 & 1 == 1； 011 & 10 == 1。两个取值。即ab
+		for (int offset = 0; offset < length; ++offset) {	// 对每一种bitmap偏移量offset均从0开始,按位选取判断。
+			if (bitmap & (1 << offset))	//bitmap=011,则j通过for循环依次取0和1，011 & 1 == 1； 011 & 10 == 1。两个取值。即ab
 				cout << *(str + offset);
 		}
 		cout << endl;
